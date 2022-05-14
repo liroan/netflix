44db275d8edd3d1a4ee95374de3deefa558ab921
Denis <serzhantov-02@mail.ru>
2022-05-14 22:40:44 +0500

    прописал interface в FilmInfo

--- src/components/FilmInfo/FilmInfo.tsx
+++ src/components/FilmInfo/FilmInfo.tsx
@@ -1,27 +1,38 @@
 import next from "../../img/next.png";
 import info from "../../img/info.png";
+import {FC} from "react";
 
+interface IFilmInfo {
+    title: string,
+    rating: number,
+    year: number,
+    genre: string[],
+    country: string[],
+    restriction: number,
+    time: string,
+    marks: string[],
+    desc: string
+}
 
-const FilmInfo = () => {
+const FilmInfo:FC<IFilmInfo> = ({title, rating, year, genre, country, restriction, time, marks, desc}) => {
     return (
         <div className="mainPage__popular-film popular-film">
             <div className="popular-film__mask">
                 <div className="popular-film__container">
-                    <div className="popular-film__title"><h1>Джанго освобожденный</h1></div>
+                    <div className="popular-film__title"><h1>{title}</h1></div>
                     <div className="popular-film__info">
                         <div className="popular-film__ratings">
-                            <p>5.5</p>
+                            <p>{rating}</p>
                         </div>
                         <div className="popular-film__other-info">
-                            <p>2021, ужасы Испания 1ч 39мин 18+</p>
+                            <p>{year},{genre.map(el => <span>{el}</span>)} {country.map(el => <span>{el}</span>)} {time} {restriction}+</p>
                         </div>
                     </div>
                     <div className="popular-film__marks">
-                        <div className="popular-film__mark mark"><p>Full HD</p></div>
-                        <div className="popular-film__mark mark"><p>5.1</p></div>
+                        {marks.map(mark => <div className="popular-film__mark mark"><p>{mark}</p></div>)}
                     </div>
                     <div className="popular-film__desc">
-                        <p>Модель ставит карьеру на паузу, чтобы ухаживать за бабушкой. Испанский хоррор о больших тайнах маленькой семьи</p>
+                        <p>{desc}</p>
                     </div>
                     <div className="popular-film__buttons">
                         <div className="popular-film__button red">
--- src/components/MainPage/MainPage.tsx
+++ src/components/MainPage/MainPage.tsx
@@ -1,14 +1,22 @@
 import "./MainPage.sass"
-import info from "../../img/info.png"
-import next from "../../img/next.png"
-import {useRef} from "react";
-import Slider from "../Slider/Slider";
+import {FC} from "react";
 import FilmLine from "../FilmLine/FilmLine";
 import FilmInfo from "../FilmInfo/FilmInfo";
-const MainPage = () => {
+const MainPage:FC = () => {
+    let film = {
+        title: "Джанго освобожденный",
+        rating: 5.5,
+        year: 2021,
+        genre: ["ужасы"],
+        country: ["Испания"],
+        restriction: 18,
+        time: "1ч 39мин",
+        marks: ["Full HD", "5.5"],
+        desc: "Модель ставит карьеру на паузу, чтобы ухаживать за бабушкой. Испанский хоррор о больших тайнах маленькой семьи"
+    }
     return (
         <section className={"mainPage"}>
-            <FilmInfo />
+            <FilmInfo {...film}/>
             <div className="mainPage__lines-proposed-film lines-proposed-film">
                 <FilmLine />
             </div>

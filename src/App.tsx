fe3cb34ddce0763e8a867cb7b94460abb338d3b3
Denis <serzhantov-02@mail.ru>
2022-05-15 19:48:51 +0500

    добавил двигающийся header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import MainPage from "./components/MainPage/MainPage";
+import Header from "./components/Header/Header";
 
 const App = () => {
     return (
         <div>
+            <Header />
             <MainPage />
         </div>
     )
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,39 @@
+import "./Header.sass"
+import user from "../../img/user.png"
+import {useEffect, useState} from "react";
+const Header = () => {
+    const [prevScroll, setPrevScroll] = useState(0);
+    const [isShowHeader, setIsShowHeader] = useState(false);
+    useEffect(() => {
+        function scrollFunc(event:any) {
+            setIsShowHeader(prevScroll > window.scrollY)
+            setPrevScroll(window.scrollY)
+        }
+        window.addEventListener("scroll", scrollFunc)
+        return () => window.removeEventListener("scroll", scrollFunc);
+    }, [prevScroll])
+
+    return (
+        <header className={!isShowHeader ? "header_no-active" : (prevScroll === 0 ? "" : "header_active")}>
+            <div className="header__container">
+                <div className="header__left">
+                        <div className="header__burger"><img src="https://cdn-icons-png.flaticon.com/128/1828/1828859.png" alt=""/></div>
+                        <div className="header__logo"><img src="https://psv4.userapi.com/c235031/u254066399/docs/d3/56c45ed3750f/va.png?extra=nemoGrZleVc3Kqe8r9bvbv2GzWta0MmPbuC4-7PjTumQyP3pYvHH2XWgrBTxI0lzN6UIQlVxcdQZa6ZVTnn2DqNX-IJg1ZiNRN47kRE0gSipueA1Tzzf9yVfGe85pvgNy1aRBLIfUcgX1zlGJ6qlIlk" alt=""/></div>
+                        <nav>
+                            <ul>
+                                <li><a>Избранное</a></li>
+                                <li><a>Магазин</a></li>
+                                <li><a>Буду смотреть</a></li>
+                                <li><a><img src="https://cdn-icons.flaticon.com/png/128/3641/premium/3641364.png?token=exp=1652614657~hmac=d125e6e1401dc3851d066b08f769d1aa" alt=""/></a></li>
+                            </ul>
+                        </nav>
+                </div>
+                <div className="header__user">
+                    <img src={user} alt=""/>
+                </div>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
